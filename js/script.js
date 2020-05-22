5e5adfc7a22d615fb89470d56d441a55917586fd
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2020-05-22 13:45:42 +0300

    Реализация показа модального окна с формой обратной связи
    Пока не добавлены: валидация формы, автоматический перевод фокуса на форму, работа с LocalStorage.  Будет добавлено позже.

--- js/script.js
+++ js/script.js
@@ -23,3 +23,39 @@ window.addEventListener("keydown", function(evt) {
     search_form.style.display = "none";
   };
 });
+
+
+
+
+var feedbackButton = document.querySelector(".gllacy-contacts__button");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+var overlay = document.querySelector(".overlay");
+
+feedbackButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  overlay.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      overlay.classList.remove("modal-show");
+    }
+  }
+});
