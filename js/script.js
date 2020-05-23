afd33dfbad1c009713796ccc22f19684b4572410
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2020-05-24 02:10:27 +0300

    Реализация слайдера мороженок на главной. Прочие мелкие доработки
    Скрипт смены слайдов с мороженками пока сделан очень топорно. Сейчас была проверена только сама возможность смены слайдов, поиск возможных косяков. Косяки были найдены. Далее скрипт будет оптимизирован. Возможно, весь код слайдера будет перещен из MAIN в HEADER, чтобы не добавлять лишние классы в тег BODY.

--- js/script.js
+++ js/script.js
@@ -37,3 +37,56 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+
+
+
+
+var slideButton1 = document.querySelector(".slide-switcher__item-1");
+var slideButton2 = document.querySelector(".slide-switcher__item-2");
+var slideButton3 = document.querySelector(".slide-switcher__item-3");
+var indexBody = document.querySelector(".index-body");
+var iceSlide1 = document.querySelector(".ice-slide--1");
+var iceSlide2 = document.querySelector(".ice-slide--2");
+var iceSlide3 = document.querySelector(".ice-slide--3");
+
+slideButton1.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  indexBody.classList.remove("index-body--2");
+  indexBody.classList.remove("index-body--3");
+  iceSlide2.classList.remove("slide-show");
+  iceSlide3.classList.remove("slide-show");
+  slideButton2.classList.remove("slide-switcher__item--active");
+  slideButton3.classList.remove("slide-switcher__item--active");
+  indexBody.classList.add("index-body--1");
+  iceSlide1.classList.add("slide-show");
+  slideButton1.classList.add("slide-switcher__item--active");
+});
+
+slideButton2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  indexBody.classList.remove("index-body--1");
+  indexBody.classList.remove("index-body--3");
+  iceSlide1.classList.remove("slide-show");
+  iceSlide3.classList.remove("slide-show");
+  slideButton1.classList.remove("slide-switcher__item--active");
+  slideButton3.classList.remove("slide-switcher__item--active");
+  indexBody.classList.add("index-body--2");
+  iceSlide2.classList.add("slide-show");
+  slideButton2.classList.add("slide-switcher__item--active");
+});
+
+slideButton3.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  indexBody.classList.remove("index-body--1");
+  indexBody.classList.remove("index-body--2");
+  iceSlide1.classList.remove("slide-show");
+  iceSlide2.classList.remove("slide-show");
+  slideButton1.classList.remove("slide-switcher__item--active");
+  slideButton2.classList.remove("slide-switcher__item--active");
+  indexBody.classList.add("index-body--3");
+  iceSlide3.classList.add("slide-show");
+  slideButton3.classList.add("slide-switcher__item--active");
+});
+
+
