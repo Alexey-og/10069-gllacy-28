c02fd25914cfacbb426f6869ff1b4ffcd3525c4c
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2020-05-23 17:32:12 +0300

    Добавление интерактивной карты
    Со шрифтами пока не получилось разобраться, почему не срабатывает Roboto вместо Arial

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
 'use strict';
 
-var search_icon = document.querySelector(".search__icon");
-var search_form = document.querySelector(".search__form");
-var search_input = document.querySelector(".search__input");
-
-search_icon.addEventListener("focus", function(evt) {
-  evt.preventDefault;
-  /* search_icon.style.backgroundColor = "#ffffff"; */
-  /* search_icon.style.backgroundImage = "url('../img/icon-search-black.svg')"; */
-  search_form.style.display = "flex";
-  search_input.focus();
-});
-
-search_input.addEventListener("focusout", function(evt) {
-  evt.preventDefault;
-  search_form.style.display = "none";
-});
-
-window.addEventListener("keydown", function(evt) {
-  if (evt.key === "Escape") {
-    evt.preventDefault;
-    search_form.style.display = "none";
-  };
-});
+document.querySelector(".gllacy-map__pin").classList.add("visually-hidden");
 
 
 
