1c5a22609ad6edacb88f98cfcff1deb350d82495
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2020-06-10 18:03:22 +0300

    Начало по внедрению доступности по табам. Продолжение работы с JS
    Попытки настройки страниц, чтобы можно было совершать навигацию только с помощью клавиатуры.
    
    Начало внедрения вспомогательных скриптов для работы с формами.
    
    Добавлена анимация появления попапа с фидбеком.

--- js/index.js
+++ js/index.js
@@ -56,3 +56,96 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
+
+
+
+var loginIcon = document.querySelector(".login__icon");
+var loginForm = document.querySelector(".login__form");
+var loginEmail = loginForm.querySelector("[name=email]");
+var loginPassword = loginForm.querySelector("[name=password]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+loginIcon.addEventListener("focus", function (evt) {
+  evt.preventDefault();
+  loginForm.classList.add("modal-show");
+  if (storage) {
+    loginEmail.value = storage;
+    password.focus();
+  } else {
+    loginEmail.focus();
+  }
+});
+
+loginForm.addEventListener("focusout", function (evt) {
+  evt.preventDefault();
+  loginForm.classList.remove("modal-show");
+});
+
+loginForm.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  }
+});
+
+/*
+
+var link = document.querySelector(".login-link");
+var popup = document.querySelector(".modal-login");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+var isStorageSupport = true;
+var storage = "";
+try {
+storage = localStorage.getItem("login");
+} catch (err) {
+isStorageSupport = false;
+}
+link.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.add("modal-show");
+if (storage) {
+login.value = storage;
+password.focus();
+} else {
+login.focus();
+}
+});
+close.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.remove("modal-show");
+popup.classList.remove("modal-error");
+});
+form.addEventListener("submit", function (evt) {
+if (!login.value || !password.value) {
+evt.preventDefault();
+popup.classList.remove("modal-error");
+popup.offsetWidth = popup.offsetWidth;
+popup.classList.add("modal-error");
+} else {
+if (isStorageSupport) {
+localStorage.setItem("login", login.value);
+}
+}
+});
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+if (popup.classList.contains("modal-show")) {
+evt.preventDefault();
+popup.classList.remove("modal-show");
+popup.classList.remove("modal-error");
+}
+}
+});
+</script>
+ */
